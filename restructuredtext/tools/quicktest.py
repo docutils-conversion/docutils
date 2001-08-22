21b3d248a755661d38052ec3da5836a91a2599db
David Goodger <goodger@python.org>
2001-08-22 03:56:44 +0000

    - Reworked for clarity, brevity.  - Factored parsing out of formatting.

--- restructuredtext/tools/quicktest.py
+++ restructuredtext/tools/quicktest.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-# $Id: quicktest.py,v 1.3 2001/08/03 01:19:37 gtk Exp $
-# 
+# $Id: quicktest.py,v 1.4 2001/08/22 03:56:44 goodger Exp $
+#
 # I used to have a much prettier version of this script, but
 # inadvertently deleted it. Oops. Here's a quick, ugly, grotty hack
 # as a temporary replacement. --gtk
 
 """\
-quicktest.py: quickly test the restructuredtext parser. 
+quicktest.py: quickly test the restructuredtext parser.
 
-Usage: quicktest.py [-x|-p|-t] [-q] [filename] 
+Usage: quicktest.py [-x|-p|-t] [-q] [filename]
 
     filename     : filename to use as input (default is stdin)
     -x --xml     : output raw xml
@@ -18,30 +18,33 @@ Usage: quicktest.py [-x|-p|-t] [-q] [filename]
 """
 
 """
-Author: Garth Kidd
-Contact: garth@deadlybloodyserious.com
-Revision: $Revision: 1.3 $
-Date: $Date: 2001/08/03 01:19:37 $
-Copyright: This module has been placed in the public domain.
+:Author: Garth Kidd
+:Contact: garth@deadlybloodyserious.com
+:Revision: $Revision: 1.4 $
+:Date: $Date: 2001/08/22 03:56:44 $
+:Copyright: This module has been placed in the public domain.
 
 """
 
 import sys
-import dps.parsers.restructuredtext
 import getopt
+try:
+    from restructuredtext import Parser
+except ImportError:
+    from dps.parsers.restructuredtext import Parser
 
-def usage(): 
+def usage():
     print __doc__
 
-def _parse_pretty(parser, text):
-    return parser.parse(text).pprint()
+def _pretty(input, document):
+    return document.pprint()
 
-def _parse_xml(parser, text):
-    return parser.parse(text).asdom().toxml()
+def _xml(input, document):
+    return document.asdom().toxml()
 
-def _parse_test(parser, text):
+def _test(input, document):
     tq = '"""'
-    output = _parse_pretty(parser, text)
+    output = _pretty(document)
     return """\
     proven['change_this_test_name'] = [
 [%s\\
@@ -50,21 +53,20 @@ def _parse_test(parser, text):
 %s\\
 %s
 %s],
-]""" % ( tq, text, tq, tq, output, tq )
+]""" % ( tq, input, tq, tq, output, tq )
 
-_outputHandlers = {
-    'xml': _parse_xml,
-    'pretty' : _parse_pretty,
-    'test': _parse_test
+_outputFormatters = {
+    'xml': _xml,
+    'pretty' : _pretty,
+    'test': _test
     }
 
-def formattedOutput(format, parser, text): 
-    handler = _outputHandlers[format]
-    return apply(handler, (parser, text))
-    
-def main(): 
+def format(outputFormat, input, document):
+    formatter = _outputFormatters[outputFormat]
+    return formatter(input, document)
+
+def main():
     outputFormat = 'pretty'
-    inputFile = sys.stdin
     isQuiet = 0
 
     try:
@@ -73,7 +75,7 @@ def main():
     except getopt.GetoptError:
         usage()
         sys.exit(2)
-    
+
     for o, a in opts:
         if o in ['-h', '--help']: # undocumented!
             usage()
@@ -88,30 +90,26 @@ def main():
             isQuiet = 1
         else:
             raise AssertionError, "getopt should have saved us!"
-    
+
     if len(args)>1:
         print "Only one file at a time, thanks."
         usage()
         sys.exit(1)
-    
+
     if len(args) == 1:
         inputFile = open(args[0])
+    else:
+        inputFile = sys.stdin
 
     # All of that hard work getting the options, when doing the work
     # couldn't possibly be simpler:
-    
-    # create a parser
-    parser = dps.parsers.restructuredtext.Parser()
 
-    # gather input
-    input = inputFile.read()
-
-    # print a delimiter
-    if not isQuiet:
+    parser = Parser()                   # create a parser
+    input = inputFile.read()            # gather input
+    if not isQuiet:                     # print a delimiter
         print '=>'
-        
-    # print the parser output
-    print formattedOutput(outputFormat, parser, input)
+    document = parser.parse(input)      # parse the input
+    print format(outputFormat, input, document) # format & print
 
 if __name__ == '__main__':
     main()
