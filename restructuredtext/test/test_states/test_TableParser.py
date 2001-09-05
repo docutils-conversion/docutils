13fcf3bd180d5f86641570332a69ec93a52c35af
David Goodger <goodger@python.org>
2001-09-05 02:50:21 +0000

    - Added test data for full parse.

--- restructuredtext/test/test_states/test_TableParser.py
+++ restructuredtext/test/test_states/test_TableParser.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.1 $
-:Date: $Date: 2001/09/04 04:11:01 $
+:Revision: $Revision: 1.2 $
+:Date: $Date: 2001/09/05 02:50:21 $
 :Copyright: This module has been placed in the public domain.
 
 Tests for states.py.
@@ -26,7 +26,9 @@ totest['tables'] = [
 +-------------------------------------+
 """,
 [(0, 0, 2, 38, ['A table with one cell and one line.'])],
-()],
+([37],
+ [],
+ [[(0, 0, 1, ['A table with one cell and one line.'])]])],
 ["""\
 +--------------+--------------+
 | A table with | two columns. |
@@ -34,7 +36,10 @@ totest['tables'] = [
 """,
 [(0, 0, 2, 15, ['A table with']),
  (0, 15, 2, 30, ['two columns.'])],
-()],
+([14, 14],
+ [],
+ [[(0, 0, 1, ['A table with']),
+   (0, 0, 1, ['two columns.'])]])],
 ["""\
 +--------------+-------------+
 | A table with | two columns |
@@ -46,7 +51,12 @@ totest['tables'] = [
  (0, 15, 2, 29, ['two columns']),
  (2, 0, 4, 15, ['and']),
  (2, 15, 4, 29, ['two rows.'])],
-()],
+([14, 13],
+ [],
+ [[(0, 0, 1, ['A table with']),
+   (0, 0, 1, ['two columns'])],
+  [(0, 0, 3, ['and']),
+   (0, 0, 3, ['two rows.'])]])],
 ["""\
 +--------------------------+
 | A table with three rows, |
@@ -61,7 +71,14 @@ totest['tables'] = [
  (2, 0, 4, 13, ['and two']),
  (2, 13, 4, 27, ['columns.']),
  (4, 0, 7, 27, ['First and last rows', 'contain column spans.'])],
-()],
+([12, 13],
+ [],
+ [[(0, 1, 1, ['A table with three rows,']),
+   None],
+  [(0, 0, 3, ['and two']),
+   (0, 0, 3, ['columns.'])],
+  [(0, 1, 5, ['First and last rows', 'contain column spans.']),
+   None]])],
 ["""\
 +------------+-------------+---------------+
 | A table    | two rows in | and row spans |
@@ -73,7 +90,14 @@ totest['tables'] = [
  (0, 13, 2, 27, ['two rows in']),
  (0, 27, 4, 43, ['and row spans', 'to left and', 'right.']),
  (2, 13, 4, 27, ['the middle,'])],
-()],
+([12, 13, 15],
+ [],
+ [[(1, 0, 1, ['A table', 'with three', 'columns,']),
+   (0, 0, 1, ['two rows in']),
+   (1, 0, 1, ['and row spans', 'to left and', 'right.'])],
+  [None,
+   (0, 0, 3, ['the middle,']),
+   None]])],
 ["""\
 +------------+-------------+---------------+
 | A table |  | two rows in | and funny     |
@@ -85,7 +109,14 @@ totest['tables'] = [
  (0, 13, 2, 27, ['two rows in']),
  (0, 27, 4, 43, [' and funny', '-+ stuff.', ' |']),
  (2, 13, 4, 27, ['the middle,'])],
-()],
+([12, 13, 15],
+ [],
+ [[(1, 0, 1, ['A table |', 'with 3  +--', 'columns,']),
+   (0, 0, 1, ['two rows in']),
+   (1, 0, 1, [' and funny', '-+ stuff.', ' |'])],
+  [None,
+   (0, 0, 3, ['the middle,']),
+   None]])],
 ["""\
 +-----------+-------------------------+
 | W/NW cell | N/NE cell               |
@@ -100,7 +131,17 @@ totest['tables'] = [
  (2, 12, 4, 26, ['Middle cell']),
  (2, 26, 6, 38, ['E/SE cell', '', '']),
  (4, 0, 6, 26, ['S/SE cell'])],
-()],
+([11, 13, 11],
+ [],
+ [[(1, 0, 1, ['W/NW cell', '', '']),
+   (0, 1, 1, ['N/NE cell']),
+   None],
+  [None,
+   (0, 0, 3, ['Middle cell']),
+   (1, 0, 3, ['E/SE cell', '', ''])],
+  [(0, 1, 5, ['S/SE cell']),
+   None,
+   None]])],
 ["""\
 +--------------+-------------+
 | A bad table. |             |
@@ -109,7 +150,7 @@ totest['tables'] = [
 +----------------------------+
 """,
 'MarkupError: Malformed table; parse incomplete.',
-()],
+'MarkupError: Malformed table; parse incomplete.'],
 ["""\
 +-------------------------------+
 | A table with two header rows, |
@@ -127,7 +168,15 @@ totest['tables'] = [
  (4, 0, 6, 13, ['Two body']),
  (4, 13, 6, 32, ['rows,']),
  (6, 0, 8, 32, ['the second with a span.'])],
-()],
+([12, 18],
+ [[(0, 1, 1, ['A table with two header rows,']),
+   None],
+  [(0, 0, 3, ['the first']),
+   (0, 0, 3, ['with a span.'])]],
+ [[(0, 0, 5, ['Two body']),
+   (0, 0, 5, ['rows,'])],
+  [(0, 1, 7, ['the second with a span.']),
+   None]])],
 ["""\
 +-------------------------------+
 | A table with two head/body    |
@@ -139,7 +188,8 @@ totest['tables'] = [
 """,
 'MarkupError: Multiple head/body row separators in table '
 '(at line offset 2 and 4); only one allowed.',
-()],
+'MarkupError: Multiple head/body row separators in table '
+'(at line offset 2 and 4); only one allowed.'],
 ]
 
 if __name__ == '__main__':
