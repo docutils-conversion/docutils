50b2e7d8de0885aeb1cf4c01f474acdace25fe9b
David Goodger <goodger@python.org>
2001-09-07 02:12:28 +0000

    - Changed node class names to node classes.

--- dps/dps/languages/en.py
+++ dps/dps/languages/en.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# $Id: en.py,v 1.2 2001/08/17 02:31:39 goodger Exp $
+# $Id: en.py,v 1.3 2001/09/07 02:12:28 goodger Exp $
 # by David Goodger (dgoodger@bigfoot.com)
 
 """
@@ -9,6 +9,9 @@ features.
 
 __all__ = ['parser', 'formatter']
 __docformat__ = 'reStructuredText'
+
+
+from dps import nodes
 
 
 class Stuff:
@@ -30,37 +33,39 @@ Mappings for input parsers. Attributes:
 - directives: Directive name to directive module name mapping.
 """
 
-parser.bibliofields = {'title': 'title',
-                       'author': 'author',
-                       'authors': 'authors',
-                       'organization': 'organization',
-                       'contact': 'contact',
-                       'version': 'version',
-                       'revision': 'revision',
-                       'status': 'status',
-                       'date': 'date',
-                       'copyright': 'copyright',}
+parser.bibliofields = {'title': nodes.title,
+                       'subtitle': nodes.subtitle,
+                       'author': nodes.author,
+                       'authors': nodes.authors,
+                       'organization': nodes.organization,
+                       'contact': nodes.contact,
+                       'version': nodes.version,
+                       'revision': nodes.revision,
+                       'status': nodes.status,
+                       'date': nodes.date,
+                       'copyright': nodes.copyright,
+                       'abstract': nodes.abstract}
 
 parser.authorseps = [';', ',']
 
-parser.interpreted = {'package': 'package',
-                      'module': 'module',
-                      'class': 'inline_class',
-                      'method': 'method',
-                      'function': 'function',
-                      'variable': 'variable',
-                      'parameter': 'parameter',
-                      'type': 'type',
-                      'class attribute': 'class_attribute',
-                      'classatt': 'class_attribute',
-                      'instance attribute': 'instance_attribute',
-                      'instanceatt': 'instance_attribute',
-                      'module attribute': 'module_attribute',
-                      'moduleatt': 'module_attribute',
-                      'exception class': 'exception_class',
-                      'exception': 'exception_class',
-                      'warning class': 'warning_class',
-                      'warning': 'warning_class',}
+parser.interpreted = {'package': nodes.package,
+                      'module': nodes.module,
+                      'class': nodes.inline_class,
+                      'method': nodes.method,
+                      'function': nodes.function,
+                      'variable': nodes.variable,
+                      'parameter': nodes.parameter,
+                      'type': nodes.type,
+                      'class attribute': nodes.class_attribute,
+                      'classatt': nodes.class_attribute,
+                      'instance attribute': nodes.instance_attribute,
+                      'instanceatt': nodes.instance_attribute,
+                      'module attribute': nodes.module_attribute,
+                      'moduleatt': nodes.module_attribute,
+                      'exception class': nodes.exception_class,
+                      'exception': nodes.exception_class,
+                      'warning class': nodes.warning_class,
+                      'warning': nodes.warning_class,}
 
 parser.directives = {}
 
