d7718a6dd5a4739c3cddced2c9e3007b5ffa76b9
David Goodger <goodger@python.org>
2001-09-10 04:44:21 +0000

    Implementation of a test directive, 'restructuredtext-test-directive'.

---
+++ restructuredtext/restructuredtext/directives/directivetest.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+"""
+:Author: David Goodger
+:Contact: goodger@users.sourceforge.net
+:Revision: $Revision: 1.1 $
+:Date: $Date: 2001/09/10 04:44:21 $
+:Copyright: This module has been placed in the public domain.
+
+Test directive implementation.
+"""
+
+__docformat__ = 'reStructuredText'
+
+__all__ = ['test_directive_function']
+
+def test_directive_function():
+    pass
