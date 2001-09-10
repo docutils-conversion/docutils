84f2440e5f011155bcc613bef5b4b42493880606
David Goodger <goodger@python.org>
2001-09-10 04:09:34 +0000

    - Added language module import function.

--- dps/dps/languages/__init__.py
+++ dps/dps/languages/__init__.py
@@ -1,3 +1,28 @@
 #! /usr/bin/env python
-# $Id: __init__.py,v 1.1 2001/07/22 22:35:40 goodger Exp $
-# by David Goodger (dgoodger@bigfoot.com)
+
+"""
+:Author: David Goodger
+:Contact: goodger@users.sourceforge.net
+:Revision: $Revision: 1.2 $
+:Date: $Date: 2001/09/10 04:09:34 $
+:Copyright: This module has been placed in the public domain.
+
+This package contains modules for language-dependent features of
+the Python Docstring Processing System.
+"""
+
+__docformat__ = 'reStructuredText'
+
+__all__ = ['language']
+
+_languages = {}
+
+def language(languagecode):
+    if _languages.has_key(languagecode):
+        return _languages[languagecode]
+    try:
+        module = __import__(languagecode, globals(), locals())
+    except:
+        raise
+    _languages[languagecode] = module
+    return module
