0cf1f4aee2d746c51d27ce18aa85593e221d3cd6
David Goodger <goodger@python.org>
2001-09-12 04:04:11 +0000

    - Changed name of and implemented directive_test_function.

--- restructuredtext/restructuredtext/directives/directivetest.py
+++ restructuredtext/restructuredtext/directives/directivetest.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.1 $
-:Date: $Date: 2001/09/10 04:44:21 $
+:Revision: $Revision: 1.2 $
+:Date: $Date: 2001/09/12 04:04:11 $
 :Copyright: This module has been placed in the public domain.
 
 Test directive implementation.
@@ -12,7 +12,25 @@ Test directive implementation.
 
 __docformat__ = 'reStructuredText'
 
-__all__ = ['test_directive_function']
+__all__ = ['directive_test_function']
 
-def test_directive_function():
-    pass
+
+from restructuredtext import states
+from dps import nodes
+
+
+def directive_test_function(match, typename, data, state, statemachine):
+    atts = {'type': typename}
+    if data:
+        atts['data'] = data
+    try:
+        statemachine.nextline()
+        indented, indent, offset, blankfinish = statemachine.getindented()
+        text = '\n'.join(indented)
+    except IndexError:
+        text = ''
+        blankfinish = 1
+    directivenode = nodes.directive(text, **atts)
+    if text:
+        state.nestedparse(indented, offset, directivenode)
+    return [directivenode], blankfinish
