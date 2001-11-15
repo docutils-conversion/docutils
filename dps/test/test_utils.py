e6f0f1d3603e04fb25d65faa28abb53e43d7ebf8
David Goodger <goodger@python.org>
2001-11-15 02:58:43 +0000

    Added 'AttributeParserTests'.

--- dps/test/test_utils.py
+++ dps/test/test_utils.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.1 $
-:Date: $Date: 2001/09/17 04:11:02 $
+:Revision: $Revision: 1.2 $
+:Date: $Date: 2001/11/15 02:58:43 $
 :Copyright: This module has been placed in the public domain.
 
 Test module for utils.py.
@@ -107,6 +107,55 @@ class QuietReporterTests(unittest.TestCase):
         a severe error
 """)
         self.assertEquals(self.stream.getvalue(), '')
+
+
+class AttributeParserTests(unittest.TestCase):
+
+    def test_extractattributes(self):
+        self.assertRaises(utils.BadAttributeLineError,
+                          utils.extractattributes, ['hello'])
+        self.assertRaises(utils.BadAttributeDataError,
+                          utils.extractattributes, ['[hello]'])
+        self.assertRaises(utils.BadAttributeDataError,
+                          utils.extractattributes, ['[=hello]'])
+        self.assertRaises(utils.BadAttributeDataError,
+                          utils.extractattributes, ['[hello=]'])
+        self.assertRaises(utils.BadAttributeDataError,
+                          utils.extractattributes, ['[hello="]'])
+        self.assertRaises(utils.BadAttributeDataError,
+                          utils.extractattributes, ['[hello="something]'])
+        self.assertRaises(utils.BadAttributeDataError,
+                          utils.extractattributes,
+                          ['[hello="something"else]'])
+        output = utils.extractattributes("""\
+[att1=val1 att2=val2 att3="value number '3'"]
+[att4=val4]""".splitlines())
+        self.assertEquals(output, [('att1', 'val1'), ('att2', 'val2'),
+                                   ('att3', "value number '3'"),
+                                   ('att4', 'val4')])
+
+    attributespec = {'a': int, 'bbb': float, 'cdef': lambda x: x}
+
+    def test_assembleattributes(self):
+        input = utils.extractattributes(['[a=1 bbb=2.0 cdef=hol%s]'
+                                              % chr(224)])
+        self.assertEquals(
+              utils.assembleattributes(input, self.attributespec),
+              {'a': 1, 'bbb': 2.0, 'cdef': ('hol%s' % chr(224))})
+        input = utils.extractattributes(['[a=1 b=2.0 c=hol%s]'
+                                              % chr(224)])
+        self.assertRaises(KeyError, utils.assembleattributes,
+                          input, self.attributespec)
+        input = utils.extractattributes(['[a=1 bbb=two cdef=hol%s]'
+                                              % chr(224)])
+        self.assertRaises(ValueError, utils.assembleattributes,
+                          input, self.attributespec)
+
+    def test_parseattributes(self):
+        input = ['[a=1 bbb=2.0 cdef=hol%s]' % chr(224)]
+        self.assertEquals(
+              utils.parseattributes(input, self.attributespec),
+              {'a': 1, 'bbb': 2.0, 'cdef': ('hol%s' % chr(224))})
 
 
 if __name__ == '__main__':
