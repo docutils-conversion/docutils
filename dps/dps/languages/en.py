210ecd742cf4dae75cef8312f6edf23e63e9866a
David Goodger <goodger@python.org>
2002-01-16 02:50:12 +0000

    - Added 'bibliographic_fields' and 'author_separators' (transform    support).

--- dps/dps/languages/en.py
+++ dps/dps/languages/en.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.4 $
-:Date: $Date: 2001/09/10 04:10:40 $
+:Revision: $Revision: 1.5 $
+:Date: $Date: 2002/01/16 02:50:12 $
 :Copyright: This module has been placed in the public domain.
 
 English-language mappings for language-dependent features of the Python
@@ -13,7 +13,8 @@ Docstring Processing System.
 
 __docformat__ = 'reStructuredText'
 
-__all__ = ['interpreted', 'bibliographic_labels']
+__all__ = ['interpreted', 'bibliographic_labels', 'bibliographic_fields',
+           'author_separators']
 
 
 from dps import nodes
@@ -52,3 +53,21 @@ bibliographic_labels = {
       'copyright': 'Copyright',
       'abstract': 'Abstract'}
 """Mapping of bibliographic node class name to label text."""
+
+bibliographic_fields = {
+      'author': nodes.author,
+      'authors': nodes.authors,
+      'organization': nodes.organization,
+      'contact': nodes.contact,
+      'version': nodes.version,
+      'revision': nodes.revision,
+      'status': nodes.status,
+      'date': nodes.date,
+      'copyright': nodes.copyright,
+      'abstract': nodes.abstract}
+"""Field name (lowcased) to node class name mapping for bibliographic fields
+(field_list)."""
+
+author_separators = [';', ',']
+"""List of separator strings for the 'Authors' bibliographic field. Tried in
+order."""
