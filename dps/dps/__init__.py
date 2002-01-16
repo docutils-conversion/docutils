af188cd785747240d4a60ca853658406c1502847
David Goodger <goodger@python.org>
2002-01-16 02:51:47 +0000

    - Added docstring.

--- dps/dps/__init__.py
+++ dps/dps/__init__.py
@@ -1,3 +1,45 @@
 #! /usr/bin/env python
-# $Id: __init__.py,v 1.1 2001/07/22 22:35:43 goodger Exp $
-# by David Goodger
+
+"""
+:Author: David Goodger
+:Contact: goodger@users.sourceforge.net
+:Revision: $Revision: 1.2 $
+:Date: $Date: 2002/01/16 02:51:47 $
+:Copyright: This module has been placed in the public domain.
+
+This is the Docstring Processing System (DPS) Python package. 
+
+Package Structure
+=================
+
+Modules:
+
+- __init__.py: Contains package docstring only (this text).
+
+- nodes.py: DPS document tree (doctree) node class library.
+
+- roman.py: Conversion to and from Roman numerals. Courtesy of Mark
+  Pilgrim (http://diveintopython.org/).
+
+- statemachine.py: A finite state machine specialized for
+  regular-expression-based text filters.
+
+- urischemes.py: Contains a complete mapping of known URI addressing
+  scheme names to descriptions.
+
+- utils.py: Miscellaneous utilities.
+
+Subpackages:
+
+- languages: Language-specific mappings of terms.
+
+- parsers: Syntax-specific input parser modules or packages.
+
+- readers: Context-specific input handlers which understand the data
+  source and manage a parser.
+
+- transforms: Modules used by readers and writers to modify DPS
+  doctrees.
+
+- writers: Format-specific output translators.
+"""
