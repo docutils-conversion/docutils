d60f87ae1778a29286b4e1238d31547bb0aadd00
David Goodger <goodger@python.org>
2002-01-25 23:50:55 +0000

    - Removed Python-specific interpreted text mapping.

--- dps/dps/languages/en.py
+++ dps/dps/languages/en.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.5 $
-:Date: $Date: 2002/01/16 02:50:12 $
+:Revision: $Revision: 1.6 $
+:Date: $Date: 2002/01/25 23:50:55 $
 :Copyright: This module has been placed in the public domain.
 
 English-language mappings for language-dependent features of the Python
@@ -19,27 +19,6 @@ __all__ = ['interpreted', 'bibliographic_labels', 'bibliographic_fields',
 
 from dps import nodes
 
-
-interpreted = {
-      'package': nodes.package,
-      'module': nodes.module,
-      'class': nodes.inline_class,
-      'method': nodes.method,
-      'function': nodes.function,
-      'variable': nodes.variable,
-      'parameter': nodes.parameter,
-      'type': nodes.type,
-      'class attribute': nodes.class_attribute,
-      'classatt': nodes.class_attribute,
-      'instance attribute': nodes.instance_attribute,
-      'instanceatt': nodes.instance_attribute,
-      'module attribute': nodes.module_attribute,
-      'moduleatt': nodes.module_attribute,
-      'exception class': nodes.exception_class,
-      'exception': nodes.exception_class,
-      'warning class': nodes.warning_class,
-      'warning': nodes.warning_class,}
-"""Mapping of interpreted text role name to nodes.py class."""
 
 bibliographic_labels = {
       'author': 'Author',
