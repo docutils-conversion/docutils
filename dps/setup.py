cdde1c5dea3f381f61a88ca378dbb0c779f0dd70
David Goodger <goodger@python.org>
2002-02-06 03:04:19 +0000

    Added readers, writers subpackages.

--- dps/setup.py
+++ dps/setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-# $Id: setup.py,v 1.5 2002/01/30 04:56:15 goodger Exp $
+# $Id: setup.py,v 1.6 2002/02/06 03:04:19 goodger Exp $
 
 from distutils.core import setup
 
 def do_setup():
-    dist = setup(name = 'dps',
+    dist = setup(
+          name = 'dps',
           description = 'Python Docstring Processing System',
           #long_description = '',
           url = 'http://docstring.sourceforge.net/',
@@ -12,8 +13,8 @@ def do_setup():
           author = 'David Goodger',
           author_email = 'goodger@users.sourceforge.net',
           license = 'public domain',
-          packages = ['dps', 'dps.parsers', 'dps.transforms',
-                      'dps.languages'])
+          packages = ['dps', 'dps.readers', 'dps.parsers', 'dps.writers',
+                      'dps.transforms', 'dps.languages'])
     return dist
 
 if __name__ == '__main__' :
