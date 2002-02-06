bbfa88d960adce4962afc062363a839f6dc291c8
David Goodger <goodger@python.org>
2002-02-06 02:42:18 +0000

    ``language()`` -> ``getlanguage()``

--- dps/dps/languages/__init__.py
+++ dps/dps/languages/__init__.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.2 $
-:Date: $Date: 2001/09/10 04:09:34 $
+:Revision: $Revision: 1.3 $
+:Date: $Date: 2002/02/06 02:42:18 $
 :Copyright: This module has been placed in the public domain.
 
 This package contains modules for language-dependent features of
@@ -13,16 +13,13 @@ the Python Docstring Processing System.
 
 __docformat__ = 'reStructuredText'
 
-__all__ = ['language']
+__all__ = ['getlanguage']
 
 _languages = {}
 
-def language(languagecode):
+def getlanguage(languagecode):
     if _languages.has_key(languagecode):
         return _languages[languagecode]
-    try:
-        module = __import__(languagecode, globals(), locals())
-    except:
-        raise
+    module = __import__(languagecode, globals(), locals())
     _languages[languagecode] = module
     return module
