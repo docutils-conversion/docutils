27dc02b29897857bcb83b596554973e3f9c4ccf0
David Goodger <goodger@python.org>
2002-02-12 02:27:47 +0000

    entries for "meta", "imagemap" directives

--- restructuredtext/restructuredtext/languages/en.py
+++ restructuredtext/restructuredtext/languages/en.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.6 $
-:Date: $Date: 2002/01/16 06:18:23 $
+:Revision: $Revision: 1.7 $
+:Date: $Date: 2002/02/12 02:27:47 $
 :Copyright: This module has been placed in the public domain.
 
 English-language mappings for language-dependent features of
@@ -31,6 +31,8 @@ directives = {
       'note': 'note',
       'tip': 'tip',
       'hint': 'hint',
-      'warning': 'warning',}
+      'warning': 'warning',
+      'meta': 'meta',
+      'imagemap': 'imagemap'}
 """English name to registered (in directives/__init__.py) directive name
 mapping."""
