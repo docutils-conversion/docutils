2a210d9d2edfb9b58fbc685bc5e54ac9fa54e99a
David Goodger <goodger@python.org>
2002-02-20 04:21:45 +0000

    reworked so a 'meta' class & function could coexist.

--- restructuredtext/restructuredtext/directives/html.py
+++ restructuredtext/restructuredtext/directives/html.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.2 $
-:Date: $Date: 2002/02/13 02:22:53 $
+:Revision: $Revision: 1.3 $
+:Date: $Date: 2002/02/20 04:21:45 $
 :Copyright: This module has been placed in the public domain.
 
 Directives for typically HTML-specific constructs.
@@ -31,14 +31,14 @@ def meta(match, typename, data, state, statemachine, attributes):
               block, offset, node, initialstate='MetaBody',
               blankfinish=blankfinish, statemachinekwargs=metaSMkwargs)
         if (newlineoffset - offset) != len(block): # incomplete parse of block?
-            sw = statemachine.memo.reporter.error(
+            msg = statemachine.memo.reporter.error(
                   'Invalid meta directive at line %s.'
                   % statemachine.abslineno())
-            node += sw
+            node += msg
     else:
-        sw = statemachine.memo.reporter.error(
+        msg = statemachine.memo.reporter.error(
               'Empty meta directive at line %s.' % statemachine.abslineno())
-        node += sw
+        node += msg
     return node.getchildren(), blankfinish
 
 def imagemap(match, typename, data, state, statemachine, attributes):
@@ -47,40 +47,37 @@ def imagemap(match, typename, data, state, statemachine, attributes):
 
 class MetaBody(states.SpecializedBody):
 
+    class meta(nodes.Special, nodes.PreBibliographic, nodes.Element):
+        """HTML-specific "meta" element."""
+        pass
+
     def fieldmarker(self, match, context, nextstate):
         """Meta element."""
-        meta, blankfinish = self.parsemeta(match)
-        self.statemachine.node += meta
+        node, blankfinish = self.parsemeta(match)
+        self.statemachine.node += node
         return [], nextstate, []
 
     def parsemeta(self, match):
         name, args = self.parsefieldmarker(match)
         indented, indent, lineoffset, blankfinish = \
               self.statemachine.getfirstknownindented(match.end())
-        meta = meta_node()
-        meta['content'] = ' '.join(indented)
+        node = self.meta()
+        node['content'] = ' '.join(indented)
         try:
             attname, val = utils.extract_name_value(name)[0]
-            meta[attname.lower()] = val
+            node[attname.lower()] = val
         except utils.AttributeParsingError:
-            meta['name'] = name
+            node['name'] = name
         for arg in args:
             try:
                 attname, val = utils.extract_name_value(arg)[0]
-                meta[attname.lower()] = val
+                node[attname.lower()] = val
             except utils.AttributeParsingError, detail:
-                sw = self.statemachine.memo.reporter.error(
+                msg = self.statemachine.memo.reporter.error(
                       'Error parsing meta tag attribute "%s": %s'
                       % (arg, detail))
-                self.statemachine.node += sw
-        return meta, blankfinish
-
-
-class meta_node(nodes.Special, nodes.PreBibliographic, nodes.Element):
-
-    """HTML-specific "meta" element."""
-
-    tagname = 'meta'
+                self.statemachine.node += msg
+        return node, blankfinish
 
 
 metaSMkwargs = {'stateclasses': (MetaBody,)}
