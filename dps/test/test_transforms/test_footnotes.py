5e2d288460daf45ebbf59645449ab41c87c1ed81
David Goodger <goodger@python.org>
2002-03-07 03:50:39 +0000

    updated

--- dps/test/test_transforms/test_footnotes.py
+++ dps/test/test_transforms/test_footnotes.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.6 $
-:Date: $Date: 2002/02/20 04:29:54 $
+:Revision: $Revision: 1.7 $
+:Date: $Date: 2002/03/07 03:50:39 $
 :Copyright: This module has been placed in the public domain.
 
 Tests for dps.transforms.references.Footnotes.
@@ -28,22 +28,6 @@ def suite():
 totest = {}
 
 totest['footnotes'] = ((Footnotes,), [
-["""\
-[label]_
-
-.. [label] text
-""",
-"""\
-<document>
-    <paragraph>
-        <footnote_reference refname="label">
-            label
-    <footnote id="id1" name="label">
-        <label>
-            label
-        <paragraph>
-            text
-"""],
 ["""\
 [#autolabel]_
 
@@ -202,7 +186,8 @@ Mixed anonymous and labelled auto-numbered footnotes:
         <footnote_reference auto="1" refname="3">
             3
          should be 3, \n\
-        <footnote_reference auto="1">
+        <problematic>
+            [#]_
          is one too many,
         <footnote_reference auto="1" refname="two">
             2
@@ -257,22 +242,19 @@ Mixed auto-numbered and manual footnotes:
 <document>
     <paragraph>
         Mixed auto-numbered and manual footnotes:
-    <footnote dupname="1" id="id1">
+    <footnote id="id1" name="1">
         <label>
             1
         <paragraph>
             manually numbered
-    <footnote auto="1" dupname="1" id="id3">
-        <label>
-            1
-        <paragraph>
-            auto-numbered
-        <system_message level="2" type="WARNING">
-            <paragraph>
-                Duplicate explicit target name: "1"
-    <footnote auto="1" id="id2" name="label">
+    <footnote auto="1" id="id3" name="2">
         <label>
             2
+        <paragraph>
+            auto-numbered
+    <footnote auto="1" id="id2" name="label">
+        <label>
+            3
         <paragraph>
             autonumber-labeled
 """],
@@ -310,6 +292,229 @@ An unlabeled autonumbered footnote referece: [#]_.
             Labeled autonumbered footnote.
             Note that the footnotes are not in the same
             order as the references.
+"""],
+["""\
+Mixed manually-numbered, anonymous auto-numbered,
+and labelled auto-numbered footnotes:
+
+[#four]_ should be 4, [#]_ should be 2,
+[1]_ is 1, [3]_ is 3,
+[#]_ should be 6, [#]_ is one too many,
+[#five]_ should be 5, and [#eight]_ doesn't exist.
+
+.. [1] Manually-numbered footnote 1.
+.. [#] Auto-numbered footnote 2.
+.. [#four] Auto-numbered footnote 4.
+.. [3] Manually-numbered footnote 3
+.. [#five] Auto-numbered footnote 5.
+.. [#] Auto-numbered footnote 6.
+.. [#five] Auto-numbered footnote 5 again (duplicate).
+""",
+"""\
+<document>
+    <paragraph>
+        Mixed manually-numbered, anonymous auto-numbered,
+        and labelled auto-numbered footnotes:
+    <paragraph>
+        <footnote_reference auto="1" refname="four">
+            4
+         should be 4, \n\
+        <footnote_reference auto="1" refname="2">
+            2
+         should be 2,
+        <footnote_reference refname="1">
+            1
+         is 1, \n\
+        <footnote_reference refname="3">
+            3
+         is 3,
+        <footnote_reference auto="1" refname="6">
+            6
+         should be 6, \n\
+        <problematic>
+            [#]_
+         is one too many,
+        <footnote_reference auto="1" refname="five">
+         should be 5, and \n\
+        <footnote_reference auto="1" refname="eight">
+         doesn't exist.
+    <footnote id="id1" name="1">
+        <label>
+            1
+        <paragraph>
+            Manually-numbered footnote 1.
+    <footnote auto="1" id="id6" name="2">
+        <label>
+            2
+        <paragraph>
+            Auto-numbered footnote 2.
+    <footnote auto="1" id="id2" name="four">
+        <label>
+            4
+        <paragraph>
+            Auto-numbered footnote 4.
+    <footnote id="id3" name="3">
+        <label>
+            3
+        <paragraph>
+            Manually-numbered footnote 3
+    <footnote auto="1" dupname="five" id="id4">
+        <label>
+            5
+        <paragraph>
+            Auto-numbered footnote 5.
+    <footnote auto="1" id="id7" name="6">
+        <label>
+            6
+        <paragraph>
+            Auto-numbered footnote 6.
+    <footnote auto="1" dupname="five" id="id5">
+        <label>
+            7
+        <system_message level="2" type="WARNING">
+            <paragraph>
+                Duplicate explicit target name: "five"
+        <paragraph>
+            Auto-numbered footnote 5 again (duplicate).
+    <system_message level="3" type="ERROR">
+        <paragraph>
+            Too many autonumbered footnote references: only 2 corresponding footnotes available.
+"""],
+["""\
+Referencing a footnote by symbol [*]_.
+
+.. [*] This is an auto-symbol footnote.
+""",
+"""\
+<document>
+    <paragraph>
+        Referencing a footnote by symbol \n\
+        <footnote_reference auto="*" refid="id1">
+            *
+        .
+    <footnote auto="*" id="id1">
+        <label>
+            *
+        <paragraph>
+            This is an auto-symbol footnote.
+"""],
+["""\
+A sequence of symbol footnote references:
+[*]_ [*]_ [*]_ [*]_ [*]_ [*]_ [*]_ [*]_ [*]_ [*]_ [*]_ [*]_.
+
+.. [*] Auto-symbol footnote 1.
+.. [*] Auto-symbol footnote 2.
+.. [*] Auto-symbol footnote 3.
+.. [*] Auto-symbol footnote 4.
+.. [*] Auto-symbol footnote 5.
+.. [*] Auto-symbol footnote 6.
+.. [*] Auto-symbol footnote 7.
+.. [*] Auto-symbol footnote 8.
+.. [*] Auto-symbol footnote 9.
+.. [*] Auto-symbol footnote 10.
+.. [*] Auto-symbol footnote 11.
+.. [*] Auto-symbol footnote 12.
+""",
+"""\
+<document>
+    <paragraph>
+        A sequence of symbol footnote references:
+        <footnote_reference auto="*" refid="id1">
+            *
+         \n\
+        <footnote_reference auto="*" refid="id2">
+            \\u2020
+         \n\
+        <footnote_reference auto="*" refid="id3">
+            \\u2021
+         \n\
+        <footnote_reference auto="*" refid="id4">
+            \\u00A7
+         \n\
+        <footnote_reference auto="*" refid="id5">
+            \\u00B6
+         \n\
+        <footnote_reference auto="*" refid="id6">
+            #
+         \n\
+        <footnote_reference auto="*" refid="id7">
+            \\u2660
+         \n\
+        <footnote_reference auto="*" refid="id8">
+            \\u2663
+         \n\
+        <footnote_reference auto="*" refid="id9">
+            \\u2665
+         \n\
+        <footnote_reference auto="*" refid="id10">
+            \\u2666
+         \n\
+        <footnote_reference auto="*" refid="id11">
+            **
+         \n\
+        <footnote_reference auto="*" refid="id12">
+            \\u2020\\u2020
+        .
+    <footnote auto="*" id="id1">
+        <label>
+            *
+        <paragraph>
+            Auto-symbol footnote 1.
+    <footnote auto="*" id="id2">
+        <label>
+            \\u2020
+        <paragraph>
+            Auto-symbol footnote 2.
+    <footnote auto="*" id="id3">
+        <label>
+            \\u2021
+        <paragraph>
+            Auto-symbol footnote 3.
+    <footnote auto="*" id="id4">
+        <label>
+            \\u00A7
+        <paragraph>
+            Auto-symbol footnote 4.
+    <footnote auto="*" id="id5">
+        <label>
+            \\u00B6
+        <paragraph>
+            Auto-symbol footnote 5.
+    <footnote auto="*" id="id6">
+        <label>
+            #
+        <paragraph>
+            Auto-symbol footnote 6.
+    <footnote auto="*" id="id7">
+        <label>
+            \\u2660
+        <paragraph>
+            Auto-symbol footnote 7.
+    <footnote auto="*" id="id8">
+        <label>
+            \\u2663
+        <paragraph>
+            Auto-symbol footnote 8.
+    <footnote auto="*" id="id9">
+        <label>
+            \\u2665
+        <paragraph>
+            Auto-symbol footnote 9.
+    <footnote auto="*" id="id10">
+        <label>
+            \\u2666
+        <paragraph>
+            Auto-symbol footnote 10.
+    <footnote auto="*" id="id11">
+        <label>
+            **
+        <paragraph>
+            Auto-symbol footnote 11.
+    <footnote auto="*" id="id12">
+        <label>
+            \\u2020\\u2020
+        <paragraph>
+            Auto-symbol footnote 12.
 """],
 ])
 
