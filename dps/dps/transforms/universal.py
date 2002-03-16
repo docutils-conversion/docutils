9cc9b61e415d8af252196538225d586e935a9e05
David Goodger <goodger@python.org>
2002-03-16 05:58:54 +0000

    - Added "Pending" transforms.  - API update.

--- dps/dps/transforms/universal.py
+++ dps/dps/transforms/universal.py
@@ -2,8 +2,8 @@
 """
 :Authors: David Goodger, Ueli Schlaepfer
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.3 $
-:Date: $Date: 2002/03/11 03:38:56 $
+:Revision: $Revision: 1.4 $
+:Date: $Date: 2002/03/16 05:58:54 $
 :Copyright: This module has been placed in the public domain.
 
 Transforms needed by most or all documents:
@@ -26,15 +26,14 @@ class Messages(Transform):
     of the document.
     """
 
-    def transform(self, doctree):
-        self.setup_transform(doctree)
-        if len(doctree.messages) > 0:
+    def transform(self):
+        if len(self.doctree.messages) > 0:
             section = nodes.section(CLASS='system-messages')
             # @@@ get this from the language module?
             section += nodes.title('', 'Docutils System Messages')
-            section += doctree.messages.getchildren()
-            doctree.messages[:] = []
-            doctree += section
+            section += self.doctree.messages.getchildren()
+            self.doctree.messages[:] = []
+            self.doctree += section
 
 
 class TestMessages(Transform):
@@ -43,22 +42,60 @@ class TestMessages(Transform):
     Append all system messages to the end of the doctree.
     """
 
-    def transform(self, doctree):
-        self.setup_transform(doctree)
-        doctree += doctree.messages.getchildren()
+    def transform(self):
+        self.doctree += self.doctree.messages.getchildren()
+
+
+class Pending(Transform):
+
+    """
+    Execute pending transforms.
+    """
+
+    stage = None
+    """The stage of processing applicable to this transform; match with
+    `nodes.pending.stage`.  Possible values include 'first_reader',
+    'last_reader', 'first_writer', and 'last_writer'.  Override in
+    subclasses."""
+
+    def transform(self):
+        for pending in self.doctree.pending:
+            if pending.stage == self.stage:
+                pending.transform(self.doctree, pending).transform()
+
+
+class FirstReaderPending(Pending):
+
+    stage = 'first_reader'
+
+
+class LastReaderPending(Pending):
+
+    stage = 'last_reader'
+
+
+class FirstWriterPending(Pending):
+
+    stage = 'first_writer'
+
+
+class LastWriterPending(Pending):
+
+    stage = 'last_writer'
+
 
 
 test_transforms = (TestMessages,)
-"""Tuple of universal transforms to apply to the raw doctree when testing."""
+"""Universal transforms to apply to the raw doctree when testing."""
 
-first_reader_transforms = ()
-"""Tuple of universal transforms to apply before any other Reader transforms."""
+first_reader_transforms = (FirstReaderPending,)
+"""Universal transforms to apply before any other Reader transforms."""
 
-last_reader_transforms = (Messages,)
-"""Tuple of universal transforms to apply after all other Reader transforms."""
+last_reader_transforms = (LastReaderPending, Messages)
+"""Universal transforms to apply after all other Reader transforms."""
 
-first_writer_transforms = ()
-"""Tuple of universal transforms to apply before any other Writer transforms."""
+first_writer_transforms = (FirstWriterPending,)
+"""Universal transforms to apply before any other Writer transforms."""
 
-last_writer_transforms = ()
-"""Tuple of universal transforms to apply after all other Writer transforms."""
+last_writer_transforms = (LastWriterPending,)
+"""Universal transforms to apply after all other Writer transforms."""
