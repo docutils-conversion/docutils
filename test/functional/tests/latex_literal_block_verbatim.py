a4efe4fbe7d4fa1a3d291cf50ec34ea7ed7f8325
Günter Milde <milde@users.sf.net>
2020-08-28 20:07:40 +0000

    LaTeX package/stylesheet for Docutils.
    New LaTeX style file with required Docutils-specific definitions.
    This file will also be published on CTAN.
    
    When "docutils.sty" is not found in the "stylesheet" setting(s),
    the LaTeX writer will insert required code into the
    document preamble (like up to this commit).
    
    Subversion-Revision: r8545

--- test/functional/tests/latex_literal_block_verbatim.py
+++ test/functional/tests/latex_literal_block_verbatim.py
@@ -1,6 +1,6 @@
 # Source and destination file names.
 test_source = "latex_literal_block.txt"
-test_destination = "latex_literal_block_verbatimtab.tex"
+test_destination = "latex_literal_block_verbatim.tex"
 
 # Keyword parameters passed to publish_file.
 reader_name = "standalone"
@@ -12,4 +12,4 @@ settings_overrides['syntax_highlight'] = 'none'
 settings_overrides['legacy_class_functions'] = False
 settings_overrides['stylesheet'] = 'docutils'
 
-settings_overrides['literal_block_env'] = 'verbatimtab'
+settings_overrides['literal_block_env'] = 'verbatim'
